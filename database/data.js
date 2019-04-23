624ec5dac06e072cc6c04ed2ae655e56a7ce6d26
Ike Okonkwo <paschal83@gmail.com>
2019-04-23 00:06:04 -0700

    fixed lint issues and verified code output

--- database/data.js
+++ database/data.js
@@ -6,61 +6,58 @@ const csvWriter = require('csv-write-stream');
 faker.seed(123);
 
 // generate data
-const generateBookInfo = function BookInfo(){
+const generateBookInfo = function BookInfo() {
   const bookInfo = {
-      title: faker.random.words(3),
-      author: faker.name.findName(),
-      description: faker.lorem.sentences(6),
-      num_reviews: faker.random.number({ 'min': 1, 'max': 50000 }),
-      num_ratings: faker.random.number({ 'min': 1, 'max': 2000000 }),
-      ratings_star: faker.random.number(0, 5, 2),
-      image: faker.image.image()
+    title: faker.random.words(3),
+    author: faker.name.findName(),
+    description: faker.lorem.sentences(6),
+    num_reviews: faker.random.number({ min: 1, max: 50000 }),
+    num_ratings: faker.random.number({ min: 1, max: 2000000 }),
+    ratings_star: faker.random.number(0, 5, 2),
+    image: faker.image.image(),
   };
   return bookInfo;
 };
 
-const generateUsers = function Users(){
+const generateUsers = function Users() {
   const users = {
     email: faker.internet.email(),
-    bookInfo_id: faker.random.number({ 'min': 1, 'max': 1000000 })
+    bookInfo_id: faker.random.number({ min: 1, max: 1000000 }),
   };
   return users;
 };
 
-
-const generateReadStatus = function ReadStatus(){
+const generateReadStatus = function ReadStatus() {
   const readStatus = {
-    bookInfo_id: faker.random.number({ 'min' :1, 'max': 1000000 }),
+    bookInfo_id: faker.random.number({ min: 1, max: 1000000 }),
     user_id: faker.random.uuid(),
-    status: faker.random.number({'min': 0, 'max': 1 })
+    status: faker.random.number({ min: 0, max: 1 }),
   };
   return readStatus;
 };
 
-
-const dataWriteToDisk = function WriteToDisk(fileName, batch, callback, drainfunc){
+const dataWriteToDisk = function WriteToDisk(fileName, batch, callback, drainfunc) {
   // get header for each file
-  let writer = csvWriter({
-    headers: Object.keys(callback())
+  const writer = csvWriter({
+    headers: Object.keys(callback()),
   });
 
   writer.pipe(fs.createWriteStream(`./data/${fileName}_${batch}.csv`));
 
-   // generate 10M records
-  var noOfProfiles =  10000;
+  // generate 10M records
+  let noOfProfiles = 10000000;
   function drainWrite() {
-    var ok = true;
+    let ok = true;
     do {
-      data = callback()
+      const data = callback();
       noOfProfiles -= 1;
       if (noOfProfiles === 0) {
         // last time!
-          writer.write(data, drainfunc);
-
+        writer.write(data, drainfunc);
       } else {
         // see if we should continue, or wait
         // don't pass the callback, because we're not done yet.
-          ok = writer.write(data);
+        ok = writer.write(data);
       }
     } while (noOfProfiles > 0 && ok);
     if (noOfProfiles > 0) {
@@ -68,7 +65,7 @@ const dataWriteToDisk = function WriteToDisk(fileName, batch, callback, drainfun
       // write some more once it drains
       writer.once('drain', drainWrite);
     }
-  };
+  }
   drainWrite();
   console.log(`SUCCESS : File ./data/${fileName}_${batch}.csv created`);
 };
@@ -78,12 +75,12 @@ console.time('timing seed');
 
 const filesToCreate = 15;
 
-for (let batch = 0; batch < 2 ; batch++) {
+for (let batch = 0; batch < 2; batch++) {
   dataWriteToDisk('readStatus', 0, generateReadStatus);
   dataWriteToDisk('users', 0, generateUsers);
 }
 
-for (let batch = 0; batch < filesToCreate ; batch++) {
+for (let batch = 0; batch < filesToCreate; batch++) {
   dataWriteToDisk('bookInfo', batch, generateBookInfo);
 }
 console.timeEnd('timing seed');
